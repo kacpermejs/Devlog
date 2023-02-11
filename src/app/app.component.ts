b382a184de99f0bf5ab80394688934531812cc62
kacpermejs <kacpermejs@gmail.com>
2023-02-11 21:43:06 +0100

    node version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'devlog-ng-frontend';
+  title = 'devlog-ng-frontend test node version changed';
 }
