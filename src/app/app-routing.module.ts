0220e6849374b99548d123e40c92c623544891a2
kacpermejs <kacpermejs@gmail.com>
2023-02-12 18:04:22 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home-page/home-page.module')
+      .then(m => m.HomePageModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
