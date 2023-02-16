849a5aa5cbcb07523452084d3180a8b49bda187c
kacpermejs <kacpermejs@gmail.com>
2023-02-16 16:48:18 +0100

    test of decryption

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,9 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AppComponent {
   constructor(private modalService: NgbModal) {
+    console.log("app id:");
+    console.log(environment.firebase.projectId);
+    console.log("Production mode: " + environment.production);
   }
 
   public open(modal: any): void {
